06488bc59c0f4685113a2584e48a0a6075222dba
hojun <hjlee9303@gmail.com>
2023-01-01 23:41:09 +0900

    Algo: Backtracking 기본문제  - 탐색 중 저장이 필요한 데이터는 저장  - Backtracking 중 base condition을 만나면 답 구하기  - Backtracking 할 때 데이터의 디폴트 값 고려

---
+++ src/backtracking/Baek15649.java
@@ -0,0 +1,62 @@
+package backtracking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Iterator;
+import java.util.StringTokenizer;
+
+// N과 M(1) 백트래킹 기본 문제
+public class Baek15649 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		isSelected = new boolean[N+1];
+		selected = new int[N+1];
+		
+		ans = new StringBuilder();
+		
+		backtracking(0);
+		
+		System.out.println(ans.toString());
+	}
+
+	// 현재 중복없이 (selectIdx+1) 개 만큼 골랐을 때 수열들을 구하기
+	static int N;
+	static int M;
+	
+	// isSelected[i]: 자연수 i를 선택했는지 안했는지 true, false
+	static boolean[] isSelected;
+	
+	// selected[idx]: 백트래킹 중 만났던 자연수 순서대로 배열에 저장
+	static int[] selected;
+	static StringBuilder ans;
+	static void backtracking(int selectIdx) {
+		if(selectIdx == M) {
+			
+			for (int i = 0; i < M; i++) {
+				ans.append(selected[i]).append(" ");
+			}
+			ans.append("\n");
+			
+			return;
+		}
+		
+		for(int num = 1 ; num <= N ; num++) {
+			if(isSelected[num]) {
+				continue;
+			}
+			
+			isSelected[num] = true;
+			selected[selectIdx] = num;
+			backtracking(selectIdx+1);
+			isSelected[num] = false;
+		}
+	}
+}
