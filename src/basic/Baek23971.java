7060e511d18303971692f8ab96d1a125820993a0
hojun <hjlee9303@gmail.com>
2024-08-18 23:44:23 +0900

    [유형: Basic]

---
+++ src/basic/Baek23971.java
@@ -0,0 +1,28 @@
+package basic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baek23971 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int h = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int cnt = 0;
+        for (int i = 0; i < h; i += n + 1) {
+            for (int j = 0; j < w; j += m + 1) {
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
