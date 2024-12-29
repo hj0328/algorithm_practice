4bf71a0b2b3a13fa1ea04d5379f4dcad1512a4d8
hojun <hjlee9303@gmail.com>
2024-12-29 23:34:39 +0900

    [Math] 몫, 나머지 연산

---
+++ src/math/Baek4673.java
@@ -0,0 +1,47 @@
+package math;
+
+import java.util.Arrays;
+
+public class Baek4673 {
+
+    public static void main(String[] args) {
+
+        boolean[] isSelf = new boolean[10001];
+        Arrays.fill(isSelf,true);
+
+        for (int i = 1; i <= 10000; i++) {
+            if (isSelf[i]) {
+                setSelf(i, isSelf);
+            }
+        }
+
+        for (int i = 1; i <= 10000; i++) {
+            if (isSelf[i]) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    private static void setSelf(int i, boolean[] isSelf) {
+        if (i > 10000) {
+            return;
+        }
+
+        int val = i;
+        int add = 0;
+        while(val >= 10) {
+            add += val % 10;
+            val = val / 10;
+        }
+
+        add += val + i;
+
+
+        if (add <= 10000) {
+
+            isSelf[add] = false;
+        }
+
+        setSelf(add,isSelf);
+    }
+}
